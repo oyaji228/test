e4a9c7433eadbfefc2a0759321f70094ea5dbec2
sunao <oyaji228@gmail.com>
2019-04-15 17:44:38 +0900

    new

---
+++ 20190327_resource_check/resource_check_r2.sh
@@ -0,0 +1,13 @@
+#!/usr/local/bin/zsh
+
+python3 resource_check_r2.py uspao01karaga-nfvi_20190326/upao01karaga-nfvi_20190326.log
+python3 resource_check_r2.py uspao01karaga-nfvi_20190326/upao01naniwa-nfvi_20190326.log
+python3 resource_check_r2.py uspao01karaga-nfvi_20190326/upao03karaga-nfvi_20190326.log
+python3 resource_check_r2.py uspao01karaga-nfvi_20190326/upao05karaga-nfvi_20190326.log
+python3 resource_check_r2.py uspao01karaga-nfvi_20190326/upao07karaga-nfvi_20190326.log
+python3 resource_check_r2.py uspao01karaga-nfvi_20190326/uspao01karaga-nfvi_20190326.log
+
+
+
+
+
